ad76ce994c967339de4c4dc994cfbf4d9df12dd4
jkalafatic <josip.kalafatic@fer.hr>
2018-01-05 01:20:15 +0100

    Dodao glavne funkcionalnosti uz pomoc tvrdo kodiranih podataka

--- OneDayJob/app/src/main/java/hr/fer/opp/onedayjob/Activities/ChatActivity.java
+++ OneDayJob/app/src/main/java/hr/fer/opp/onedayjob/Activities/ChatActivity.java
@@ -1,15 +1,66 @@
 package hr.fer.opp.onedayjob.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import hr.fer.opp.onedayjob.R;
 
 public class ChatActivity extends AppCompatActivity {
+    //Intent myIntent = getIntent();
+    //String talkingTo = myIntent.getStringExtra("talkingTo");
+
+    int[] slike={R.drawable.user1, R.drawable.instrukcije};
+    String[] messages={"bok", "posao?"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        ListView messegesListView=(ListView) findViewById(R.id.listV);
+        CustomAdapter customAdapter=new CustomAdapter();
+        messegesListView.setAdapter(customAdapter);
+
+
+    }
+
+    class CustomAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return messages.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            view= getLayoutInflater().inflate(R.layout.customlayout,null);
+            ImageView imageView=(ImageView)view.findViewById(R.id.imageView3);
+            TextView textView= (TextView) view.findViewById(R.id.textView_message);
+
+            //userova slika ovisno cija je poruka
+            imageView.setImageResource(slike[i]);
+            textView.setText(messages[i]);
+
+
+            return view;
+        }
     }
 }
--- OneDayJob/app/src/main/java/hr/fer/opp/onedayjob/Activities/MailboxActivity.java
+++ OneDayJob/app/src/main/java/hr/fer/opp/onedayjob/Activities/MailboxActivity.java
@@ -4,29 +4,49 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import hr.fer.opp.onedayjob.R;
 
-public class MailboxActivity extends AppCompatActivity {
+public class MailboxActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
+    ListView usersListV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mailbox);
 
-        Button chatButton = (Button) findViewById(R.id.chat_button);
+        //Button chatButton = (Button) findViewById(R.id.chat_button);
 
-        chatButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openChat();
-            }
-        });
+        //chatButton.setOnClickListener(new View.OnClickListener() {
+            //@Override
+            //public void onClick(View view) {
+            //    openChat();
+            //}
+        //});
+
+        String[] users= {"Ivan Ivanic", "Luka Lukic", "Marija Marijanovic", "Pero Peric", "Pero Petricic", "Ana Anicic"};
+
+        usersListV=(ListView) findViewById(R.id.listV);
+
+        ArrayAdapter<String> userAdapter= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, users);
+        usersListV.setAdapter(userAdapter);
+        usersListV.setOnItemClickListener(this);
+
     }
 
-    private void openChat(){
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+        String talkingTo=(String)usersListV.getItemAtPosition(position);
+        //Toast.makeText(this, talkingTo, Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(MailboxActivity.this, ChatActivity.class);
+        intent.putExtra("takingTo", talkingTo);
         startActivity(intent);
     }
+
 }
